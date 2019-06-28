d6ca2309ede28b0839cd751114f2fca9bc607798
William A. Hood <william.arthur.hood@gmail.com>
2019-06-27 21:46:58 -0700

    Initial creation & mockup: Memoir v2

---
+++ Memoir/Memoir.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Memoir
+{
+    // Basic log segment will be called a memoir.
+    // An encapsulation might hold a stack trace, HTTP message,another memoir, anything better depicted in HTML than plain text.
+    public class Memoir
+    {
+        public Memoir()
+        {
+        }
+    }
+}
